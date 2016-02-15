3b4b0f7c5cc00f781231dac2fdd24f68874b7cd1
Cheshire <jiaojiangonghitster@gmail.com>
2016-02-15 14:57:47 +0800

    create main.go

---
+++ main.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+	"time"
+)
+
+func rootHandler(context *gin.Context) {
+	current_time := time.Now()
+	current_time.Format("20060102150405")
+	context.JSON(200, gin.H{
+		"current_time": current_time,
+	})
+}
+
+func main() {
+	router := gin.Default()
+	router.GET("/", rootHandler)
+	router.Run()
+}
